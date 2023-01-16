6d2c13db34d34c98686312c2145ee383726fe264
AbhishekPardhi <apardhi20@iitk.ac.in>
2023-01-17 01:00:55 +0530

    p1 in cpp

---
+++ assign1/P1/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+flex lex.l
+g++ -o scan lex.yy.c -ll
+# ./scan test.csv < test.335
+./scan test1.csv < public1.335
+# ./scan test2.csv < public2.335
+# ./scan test3.csv < public3.335
+# ./scan test4.csv < public4.335
