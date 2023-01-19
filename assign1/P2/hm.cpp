b706a3b5108901dd4919383a4511484dc5d06a7d
AbhishekPardhi <abhipardhi326@gmail.com>
2023-01-19 23:28:25 +0530

    yes

---
+++ assign1/P2/hm.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        cin >> str;
+        cout  << "\"" << str << "\"|";
+    }
+    int c=3;
+    int a = -c;
+    cout << a << endl;
+}
