7ff2b11f716844da448341cb1fdf7f0ba69aeaad
AbhishekPardhi <abhipardhi326@gmail.com>
2023-01-24 00:08:37 +0530

    part 2 done

--- assign1/P2/build.sh
+++ assign1/P2/build.sh
@@ -5,4 +5,4 @@ g++ -o scan lex.yy.c -ll
 ./scan test2.csv < SyntaxCheck.java
 ./scan test3.csv < test_25.java
 ./scan test4.csv < TypeCoercion.java
-# ./scan test.csv < test.java
+./scan test.csv < test.java
