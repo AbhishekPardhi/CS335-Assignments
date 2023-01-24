c30007a23ce83dcfafeb17ea34fa8dff2512ef9b
AbhishekPardhi <abhipardhi326@gmail.com>
2023-01-24 19:17:34 +0530

    modified build file

--- assign1/P1/build.sh
+++ assign1/P1/build.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+
+# NOTE: You must have (1) "lex.l" and the folder "result" (which should have all the testcases in it) in the same directory as this bash file.
+
+program_name="scan" # Replace with the name of your program
+execute_program="./"$program_name
+csv_name="_result.csv" # Replace with the name of your csv file
+testcases_folder_path="./testcases" # Replace with the path to the folder containing the input files
+result_folder_path="./result" # Replace with the path to the folder containing the result files
+
+chmod +x build.sh # Adding execute permissions for script
+
+if [ ! -d "$result_folder_path" ]; then
+  mkdir $result_folder_path
+fi
+
 flex lex.l
-g++ -o scan lex.yy.c -ll
-./scan test.csv < test.335
-# ./scan test1.csv < public1.335
-# ./scan test2.csv < public2.335
-# ./scan test3.csv < public3.335
-# ./scan test4.csv < public4.335
+g++ -o $program_name lex.yy.c -ll
+
+for file in $testcases_folder_path/*; do
+    echo "Running program with input from file: $file"
+    result_path=$result_folder_path"/"$(basename $file)$csv_name
+    $execute_program $result_path < $file
+done
